d90772718e52480ab4ee135b64f2524bef061080
albertrandi72 <albertrandi72@gmail.com>
2024-06-24 19:04:00 +0700

    update git ignore

---
+++ storybook-static/assets/Menu.stories-BVOdSe0y.js
@@ -0,0 +1,25 @@
+import{f as b}from"./index-ByS9z8xp.js";import{M as h}from"./Menu-DO5Wg2tA.js";import"./index-DuM-I7-B.js";import"./jsx-runtime-DWbWqHZ-.js";import"./index-l2PZgWEW.js";const P={title:"Demo/Menu",component:h,parameters:{layout:"centered",default:"dark"},tags:["autodocs"],argTypes:{color:{control:"color"}},args:{onClick:()=>b()}},e={args:{label:"About",color:"white",underline:!0}},r={args:{label:"Portofolio",color:"white",underline:!1}},o={args:{label:"Service",color:"white",underline:!1}},a={args:{label:"Contact",color:"white",underline:!1}};var t,s,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: "About",
+    color: "white",
+    underline: true
+  }
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var l,c,i;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: "Portofolio",
+    color: "white",
+    underline: false
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var u,m,d;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: "Service",
+    color: "white",
+    underline: false
+  }
+}`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var p,f,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: "Contact",
+    color: "white",
+    underline: false
+  }
+}`,...(g=(f=a.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const M=["About","Portofolio","Service","Contact"];export{e as About,a as Contact,r as Portofolio,o as Service,M as __namedExportsOrder,P as default};
