ce286d5fb31866a76ab4ff0638cbf6bc86e28e54
albertrandi72 <albertrandi72@gmail.com>
2024-06-26 14:26:05 +0700

    Updates

---
+++ assets/Button.stories-CxfINiiV.js
@@ -0,0 +1,5 @@
+import{f as e}from"./index-ByS9z8xp.js";import{B as s}from"./Button-BH-gyDlX.js";import"./index-DuM-I7-B.js";import"./jsx-runtime-DWbWqHZ-.js";import"./index-l2PZgWEW.js";const i={title:"Demo/Button",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:()=>e()}},r={args:{label:"Start for Free"}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: "Start for Free"
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const d=["Basic"];export{r as Basic,d as __namedExportsOrder,i as default};
